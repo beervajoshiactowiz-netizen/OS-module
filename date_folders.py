9ba098ebb00780c8538d12829f0af57b38ddce8a
Beerva Joshi <beerva.joshi@act.local>
2026-02-13 10:37:10 +0530

    updated file

--- date_folders.py
+++ date_folders.py
@@ -1,8 +1,6 @@
 import os
 from datetime import datetime,timedelta
 
-# os.mkdir(f"C:/Users/beerva.joshi/PycharmProjects/Date_folders/{datetime.now().date()}")
-
 n=datetime.now().date()
 
 last_date=datetime(2026,12,31)
@@ -14,5 +12,5 @@ print(days)
 for i in range(days):
     n+=timedelta(days=1)
     print(n)
-    # os.mkdir(f"C:/Users/beerva.joshi/PycharmProjects/Date_folders/{n}")
+    os.mkdir(f"C:/Users/beerva.joshi/PycharmProjects/Date_folders/{n}")
 
