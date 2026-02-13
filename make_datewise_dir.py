aeaf6c77ecca2e60e02abae2b91bec8f83fd703c
Beerva Joshi <beerva.joshi@act.local>
2026-02-13 15:28:13 +0530

    13th loc moved inside the function and change in function calling

--- make_datewise_dir.py
+++ make_datewise_dir.py
@@ -1,15 +1,22 @@
 import os
 from datetime import datetime,timedelta
 
-def make_dir(end_date,start_date=datetime.now().date()):
+print("Create folder from first date to last date")
+
+def make_dir(end_date,start_date):
 
     total_days_from_now=(end_date-start_date).days
 
     for i in range(total_days_from_now):
         start_date+=timedelta(days=1)
         os.mkdir(f"C:/Users/beerva.joshi/PycharmProjects/Date_folders/{start_date}")
+    print("Folders created successfully")
 
-last_date_input=input("enter in YYYY-MM-DD format: ")
+
+last_date_input=input("enter last date in yyyy-mm-dd format: ")
 last_date=datetime.strptime(last_date_input,"%Y-%m-%d").date()
+first_date_input=input("enter starting date in yyyy-mm-dd format: ")
+first_date=datetime.strptime(first_date_input,"%Y-%m-%d").date()
 
-make_dir(last_date)
+
+make_dir(last_date,first_date)
